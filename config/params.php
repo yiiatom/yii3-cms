45ec660ee4def4ece9bb741f6bf8642cbf37467d
Dmitry Khlystov <dkhlystov@gmail.com>
2026-04-20 13:15:11 +1000

    Adding params

--- config/params.php
+++ config/params.php
@@ -1,17 +1,6 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-    // Application parameters
-    'app.name' => 'Atom',
-    'app.version' => '1.0.0',
-
-    // Database configuration
-    'db.host' => 'localhost',
-    'db.name' => 'atom_cms',
-    'db.user' => 'root',
-    'db.password' => '',
-
-    // Paths
-    'paths.uploads' => '@webroot/uploads',
-    'paths.temp' => '@runtime/temp',
 ];
